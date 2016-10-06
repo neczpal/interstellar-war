4953c10118950150f256ef7ee9abd1c3ccbddf80
neczpal <neczpal@sch.bme.hu>
2016-10-06 19:59:29 +0200

    removed useless commentsn

--- src/game/ui/Window.java
+++ src/game/ui/Window.java
@@ -1,14 +1,14 @@
 package game.ui;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import game.Loader;
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.lwjgl.opengl.GL11.*;
 
@@ -17,9 +17,8 @@ import static org.lwjgl.opengl.GL11.*;
  */
 public class Window extends Thread {
 
-	private String name;
 	private final int width, height;
-
+	private String name;
 	private Panel cPanel;
 
 	public Window (String name, int width, int height) {
@@ -28,6 +27,10 @@ public class Window extends Thread {
 		this.width = width;
 		this.height = height;
 		this.cPanel = new Panel ();
+	}
+
+	public static void main (String[] args) {
+		new Window ("Interstellar War", 640, 480).start ();
 	}
 
 	@Override
@@ -49,7 +52,6 @@ public class Window extends Thread {
 		}
 		clean ();
 	}
-
 
 	private void initDisplay () {
 		try {
@@ -76,96 +78,18 @@ public class Window extends Thread {
 		glDisable (GL_DEPTH_TEST);
 	}
 
-	//    private double fok = 0.0;
 	private void draw () {
-		//        doboz.tukroz(tukor).draw();
-		//        doboz.draw();
-		//        c.draw();
-		//        c.tukroz(tukor).draw();
-		//        c.tukroz(tukor2).draw();
-		//        c.tukroz(tukor).tukroz(tukor2).draw();
-		//        c.tukroz(tukor2).tukroz(tukor).draw();
 		cPanel.draw ();
-		//        Line l1 = line.visszLine(tukor);
-		//        if(l1 != null){
-		////            Line l2 = l1.visszLine(tukor2);
-		////            if(l2 != null){
-		////                l2.draw();
-		////            }
-		//            l1.draw();
-		//        }
-		////
-		//        glColor3f(1f, 1f, 1f);
-		//        Circle c = new Circle(250, 250, 200);
-		//        Circle c2 = new Circle(420,320, 50);
-		//        Line l = new Line(p, c.metszet(c2)[1]);
-		//        Line l2 = new Line(p, c.metszet(c2)[0]);
-		//        c.draw();
-		//        c2.draw();
-		//        glColor3f(1f, 0f, 0f);
-		//        l.draw();
-		//        l2.draw();
-
-		//        t.draw();
-		//        if(c.isInside(p))
-		//            glColor3f(1f, 0f, 0f);
-		//        else
-		//            glColor3f(1f, 1f, 1f);
-		//        poly.draw();
-		//          t.draw();
-		//          glColor3f(1f, 0f, 0f);
-		//          System.mOut.println(t.getTerulet() + ": " + t.getKerulet() + ": " + t.getInnerCircle().getCenter());
-		//          t.getInnerCircle().draw();
-		//          glColor3f(1f, 1f, 1f);
-		//        t.draw();
-		//        poly.draw();
-		//        Triangle te = t.forgat(p,fok);
-		//        te.draw();
-		//        Line l3 = new Line (te.getSulyPoint(), te.getSulyPoint().tukroz(e));
-		//        l3.draw();
-		//        Line l4 = new Line (t.getA(), l3.toEgyenes().metszet(e));
-		//        l4.draw();
-		//        te.tukroz(e).tukroz(l4.toEgyenes()).draw();
-		//        te.tukroz(e).draw();
-		//        t.tukroz(e).draw();
-		//        doboz.tukroz(p).draw();
-		//        doboz.draw();
-		//        line.draw();
-		//        tukor.draw();
-		//        tukor2.draw();
-		//          Math.a
 	}
 
-	//    private boolean asd = false;
 	private void mouseEvent () {
 		if (Mouse.isInsideWindow ()) {
-			////            c.setK(Mouse.getX(), Mouse.getY());
-			////            doboz.setPosition(Mouse.getX(), Mouse.getY());
-			////        }
-			////            if(asd)
-			////                line.setB(Mouse.getX(), Mouse.getY());
-			////            else
-			////                line.setA(Mouse.getX(), Mouse.getY());
-			//            p.setPosition(Mouse.getX(), Mouse.getY());
+			cPanel.mouseEvent ();
 		}
-		//        if(Mouse.isButtonDown(0))
-		//            asd = !asd;
-		cPanel.mouseEvent ();
 	}
 
 	private void keyboardEvent () {
 		cPanel.keyboardEvent ();
-		//        if(Keyboard.isKeyDown(Keyboard.KEY_R)){
-		//            poly = Polygon.randomize(640d, 480d, 3);
-		//        }
-		//            fok += 0.4;
-		////            line = new Line((int)(Math.random()*width),(int)(Math.random()*height),(int)(Math.random()*width),(int)(Math.random()*height));
-		////            tukor = new Line ((int)(Math.random()*width),(int)(Math.random()*height),(int)(Math.random()*width),(int)(Math.random()*height));
-		////            tukor2 = new Line ((int)(Math.random()*width),(int)(Math.random()*height),(int)(Math.random()*width),(int)(Math.random()*height));
-		//        }
-		//        if(Keyboard.isKeyDown(Keyboard.KEY_E)){
-		//            fok -= 0.05;
-		//        }
 	}
 
 	private void clean () {
@@ -173,11 +97,6 @@ public class Window extends Thread {
 		Display.destroy ();
 		Keyboard.destroy ();
 		Mouse.destroy ();
-	}
-
-
-	public static void main (String[] args) {
-		new Window ("Interstellar War", 640, 480).start ();
 	}
 
 }
