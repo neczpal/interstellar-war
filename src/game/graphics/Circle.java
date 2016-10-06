e2c0182e4df567c3545de26e32521b1d3ef87ba6
neczpal <neczpal@sch.bme.hu>
2016-10-06 20:19:31 +0200

    added 2d basic graphics

---
+++ src/game/graphics/Circle.java
@@ -0,0 +1,70 @@
+package game.graphics;
+
+import static org.lwjgl.opengl.GL11.*;
+
+/**
+ * Created by neczp on 2016. 10. 06..
+ */
+public class Circle {
+	private static final int circle_points = 120;
+
+	private Point2D k;
+	private double r;
+
+	public Circle (int x, int y, int r) {
+		this (new Point2D (x, y), r);
+	}
+
+	public Circle (Point2D k, double r) {
+		this.k = k;
+		this.r = r;
+	}
+
+	//DRAW
+	public void draw () {
+		//        glMatrixMode(GL_MODELVIEW);
+		glPushMatrix ();
+		glLoadIdentity ();
+		glTranslated (k.x, k.y, 0.0d);
+		final double angle = 2.0 * Math.PI / circle_points;
+
+		glBegin (GL_POLYGON);
+		double angle1 = 0.0;
+		glVertex2d (r * Math.cos (0.0), r * Math.sin (0.0));
+		int i;
+		for (i = 0; i < circle_points; i++) {
+			glVertex2d (r * Math.cos (angle1), r * Math.sin (angle1));
+			angle1 += angle;
+		}
+		glEnd ();
+		glPopMatrix ();
+	}
+
+	public void move (int dx, int dy) {
+		k.move (dx, dy);
+	}
+
+	public void rotate (Point2D p, double angle) {
+		p.rotate (p, angle);
+	}
+
+	public boolean isInside (Point2D p) {
+		return Math.pow ((p.x - k.x), 2) + Math.pow ((p.y - k.y), 2) <= r * r;
+	}
+
+	public Point2D getCenter () {
+		return k;
+	}
+
+	public double getRadius () {
+		return r;
+	}
+
+	public void setRadius (int radius) {
+		this.r = radius;
+	}
+
+	public void setPosition (int x, int y) {
+		k.setPosition (x, y);
+	}
+}
---
+++ src/game/graphics/Point2D.java
@@ -0,0 +1,58 @@
+package game.graphics;
+
+import static org.lwjgl.opengl.GL11.*;
+
+/**
+ * Created by neczp on 2016. 10. 06..
+ */
+public class Point2D {
+	int x, y;
+
+	public Point2D () {
+		this (0, 0);
+	}
+
+	public Point2D (int x, int y) {
+		setPosition (x, y);
+	}
+
+	public void draw () {
+		glBegin (GL_POINTS);
+		{
+			glVertex2d (x, y);
+		}
+		glEnd ();
+	}
+
+	public void move (int dx, int dy) {
+		x += dx;
+		y += dy;
+	}
+
+	public void rotate (Point2D p, double angle) {
+		double lx = p.x - x;
+		double ly = p.y - y;
+		double cos = Math.cos (angle);
+		double sin = Math.sin (angle);
+
+		x = p.x - (int) (lx * cos - ly * sin);
+		y = p.y - (int) (lx * sin + ly * cos);
+	}
+
+	public double distance (Point2D p) {
+		return Math.sqrt (Math.pow (p.x - x, 2) + Math.pow (p.y - y, 2));
+	}
+
+	public void setX (int x) {
+		this.x = x;
+	}
+
+	public void setY (int y) {
+		this.y = y;
+	}
+
+	public final void setPosition (int x, int y) {
+		setX (x);
+		setY (y);
+	}
+}
