d18bc5ac062aa19fc739e6665e107a1885b2402b
neczpal <neczpal@sch.bme.hu>
2016-10-17 17:32:39 +0200

    timestamp implemented to logging

--- src/game/Log.java
+++ src/game/Log.java
@@ -9,14 +9,33 @@ public class Log {
 	}
 
 	public void e (Object object) {
-		System.err.println (mClassName + ":\tERROR:\t" + object);
+		System.err.println (getTimeStamp () + ":\t" + mClassName + ":\tERROR:\t" + object);
 	}
 
 	public void i (Object object) {
-		System.out.println (mClassName + ":\tINFO:\t" + object);
+		System.out.println (getTimeStamp () + ":\t" + mClassName + ":\tINFO:\t" + object);
 	}
 
 	public void w (Object object) {
-		System.out.println (mClassName + ":\tWARNING:\t" + object);
+		System.out.println (getTimeStamp () + ":\t" + mClassName + ":\tWARNING:\t" + object);
+	}
+
+	private String fillZeros (String s, int length) {
+		while (s.length () < length) {
+			s = "0" + s;
+		}
+		return s;
+	}
+
+	private String getTimeStamp () {
+		long time = System.currentTimeMillis ();
+		String milis = fillZeros (Long.toString (time % 1000), 3);
+		time /= 1000;
+		String secs = fillZeros (Long.toString (time % 60), 2);
+		time /= 60;
+		String mins = fillZeros (Long.toString (time % 60), 2);
+		time /= 60;
+		String hours = fillZeros (Long.toString (time % 24), 2);
+		return hours + ":" + mins + ":" + secs + "." + milis;
 	}
 }
