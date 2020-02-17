12389ff071c9ca7c893496f6cc28fdf6575a3451
neczpal <neczpal@gmail.com>
2020-02-17 19:01:56 +0100

    Game - conception in code

--- src/net/neczpal/interstellarwar/common/game/Player.java
+++ src/net/neczpal/interstellarwar/common/game/Player.java
@@ -4,26 +4,43 @@ import net.neczpal.interstellarwar.desktop.geom.Color;
 
 public class Player {
     private int mRoomIndex;
-    private Color mColor;
+    private Color mColor;//????
 
+    //amount of current money
     private int mMoney;
 
-    private Research mResearch;
+    //ATK - indices
+    private int mSpaceShipCountLevel;// 1     ->    2     ->   4
+    private int mSpaceShipSpeedLevel;// 0.35  ->    0.75  ->   1.5
+    private int mSpaceShipAttackLevel;//1.    ->    1.2   ->   1.5
 
-    //ATK
-    private int mSpaceShipCount;
-    private int mSpaceShipSpeed;
-    private int mSpaceShipAttack;
+    //DEF - indices
+    private int mShieldMaxLevel;//       30   ->    50    -> 100
+    private int mShieldSpeedLevel;//      1.  ->    1.2   ->   1.5
+    private int mShieldCostLevel;//       15  ->    12    ->   8
 
-    //DEF
-    private int mShieldMax;
-    private int mShieldSpeed;
-    private int mShieldCost;
+    //TECH - indices
+    private int mUnitRateLevel;//1.    ->    1.2   ->   1.5
+    private int mMoneyRateLevel;//1.    ->    1.2   ->   1.5
+    private int mUpgradeCostLevel;//1.    ->    0.8   ->   0.6
 
-    //TECH
-    private int mUnitRate;
-    private int mMoneyRate;
-    private int mUpgradeCost;
+    public Player (int roomIndex) {
+        mRoomIndex = roomIndex;
 
+        mMoney = 0;
+
+        mSpaceShipCountLevel = 1;
+        mSpaceShipSpeedLevel = 1;
+        mSpaceShipAttackLevel = 1;
+
+        mShieldMaxLevel = 1;
+        mShieldSpeedLevel = 1;
+        mShieldCostLevel = 1;
+
+        mUnitRateLevel = 1;
+        mMoneyRateLevel = 1;
+        mUpgradeCostLevel = 1;
+
+    }
 
 }
--- src/net/neczpal/interstellarwar/common/game/Research.java
+++ src/net/neczpal/interstellarwar/common/game/Research.java
@@ -1,4 +1,67 @@
 package net.neczpal.interstellarwar.common.game;
 
 public class Research {
+    private static Research single_instance = null;
+
+    // VALUES - START
+
+    //ATK
+    private final int[] mSpaceShipCount;// 1     ->    2     ->   4
+    private final double[] mSpaceShipSpeed;// 0.35  ->    0.75  ->   1.5
+    private final double[] mSpaceShipAttack;//1.    ->    1.2   ->   1.5
+
+    //DEF
+    private final int[] mShieldMax;//       30   ->    50    -> 100
+    private final double[] mShieldSpeed;//      1.  ->    1.2   ->   1.5
+    private final int[] mShieldCost;//       15  ->    12    ->   8
+
+    //TECH
+    private final double[] mUnitRate;//       1.    ->    1.2   ->   1.5
+    private final double[] mMoneyRate;//      1.    ->    1.2   ->   1.5
+    private final double[] mUpgradeCost;//    1.    ->    0.8   ->   0.6
+
+    //VALUES - END
+    private final int[] mCost;//#TODO Cost according to upgrade
+//    //COSTS - START
+//
+//    //ATK
+//    private final int[]    mSpaceShipCount;// 1     ->    2     ->   4
+//    private final double[] mSpaceShipSpeed;// 0.35  ->    0.75  ->   1.5
+//    private final double[] mSpaceShipAttack;//1.    ->    1.2   ->   1.5
+//
+//    //DEF
+//    private final int[]    mShieldMax;//       30   ->    50    -> 100
+//    private final double[] mShieldSpeed;//      1.  ->    1.2   ->   1.5
+//    private final int[]    mShieldCost;//       15  ->    12    ->   8
+//
+//    //TECH
+//    private final double[] mUnitRate;//       1.    ->    1.2   ->   1.5
+//    private final double[] mMoneyRate;//      1.    ->    1.2   ->   1.5
+//    private final double[] mUpgradeCost;//    1.    ->    0.8   ->   0.6
+//
+//    //COSTS - END
+
+    private Research () {
+        mSpaceShipCount = new int[] {1, 2, 4};
+        mSpaceShipSpeed = new double[] {.35, .75, 1.5};
+        mSpaceShipAttack = new double[] {1.0, 1.2, 1.5};
+
+        mShieldMax = new int[] {30, 50, 100};
+        mShieldSpeed = new double[] {1.0, 1.2, 1.5};
+        mShieldCost = new int[] {15, 12, 8};
+
+        mUnitRate = new double[] {1.0, 1.2, 1.5};
+        mMoneyRate = new double[] {1.0, 1.2, 1.5};
+        mUpgradeCost = new double[] {1.0, 0.8, 0.6};
+
+        mCost = new int[] {0, 250, 750};
+    }
+
+    public static Research getInstance () {
+        if (single_instance == null)
+            single_instance = new Research ();
+
+        return single_instance;
+    }
+
 }
